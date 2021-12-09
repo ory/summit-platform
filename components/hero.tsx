80ccfa3419f429c7148288f76ec355f582f86a8b
Vincent <vincent@ory.sh>
2021-12-09 15:29:15 +0100

    fix: add title

--- components/hero.tsx
+++ components/hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
         {SITE_DESCRIPTION}
       </h2>
       <h1 className={cn(styleUtils.appear, styleUtils['appear-third'], styles.hero)}>
-        The first {BRAND_NAME}
+        Ory Summit 2022{BRAND_NAME}
         <br className={styleUtils['show-on-desktop']} /> global user conference
       </h1>
       <h2
