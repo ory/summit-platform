449ed9392914d0060c962812fe61eef66c3e4e59
Vincent <vincent@ory.sh>
2021-12-09 15:48:53 +0100

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,19 +14,19 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://ory-events.vercel.app/';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'orycorp';
+export const BRAND_NAME = 'Ory';
+export const SITE_NAME_MULTILINE = ['Ory', 'Summit'];
+export const SITE_NAME = 'Ory Summit';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'A description of the Ory Summit 2022, TBD.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+export const DATE = 'September 27, 2022';
+export const SHORT_DATE = 'Sep 27 - 9:00am PST';
+export const FULL_DATE = 'Sep 27th 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
@@ -36,25 +36,25 @@ export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
 export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
+  'https://www.ory.sh/code-of-conduct/';
 export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
-export const SAMPLE_TICKET_NUMBER = 1234;
+export const SAMPLE_TICKET_NUMBER = 666;
 export const NAVIGATION = [
+  {
+    name: 'Main Stage',
+    route: '/stage/main'
+  },
   {
     name: 'Stage A',
     route: '/stage/a'
   },
   {
+    name: 'Stage B',
+    route: '/stage/b'
+  },
+  {
     name: 'Stage C',
     route: '/stage/c'
-  },
-  {
-    name: 'Stage M',
-    route: '/stage/m'
-  },
-  {
-    name: 'Stage E',
-    route: '/stage/e'
   },
   {
     name: 'Schedule',
