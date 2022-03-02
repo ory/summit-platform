e373326efe8e9ab547bc193442e0f5adecbec36a
vinckr <vincent@ory.sh>
2022-03-02 19:40:46 +0100

    fix: learn more

--- components/learn-more.tsx
+++ components/learn-more.tsx
@@ -22,11 +22,7 @@ import { REPO } from '@lib/constants';
 export default function LearnMore() {
   return (
     <div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.contact)}>
-      Learn more on{' '}
-      <a href={REPO} className={styles['contact-email']} target="_blank" rel="noopener noreferrer">
-        GitHub
-      </a>
-      .
+      Ory Summit 2022 is a global, free, and hybrid conference for developers
     </div>
   );
 }
