5a45dc23c98c68f1f4b90ca39af3b7673e5a8745
vinckr <vincent@ory.sh>
2022-03-11 16:02:41 +0100

    fix: landing page ordering

--- components/index.tsx
+++ components/index.tsx
@@ -52,8 +52,8 @@ export default function Conf({
             {pageState === 'registration' && !sharePage ? (
               <>
                 <Hero />
-                <Form />
                 <LearnMore />
+                <Form />
               </>
             ) : (
               <Ticket
