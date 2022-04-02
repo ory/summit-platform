9d07e24940ef3d006c3af383df24cc3cd0d51c4d
vinckr <vincent@ory.sh>
2022-04-02 14:37:52 +0200

    fix: enlarge logo

--- components/icons/icon-logo.tsx
+++ components/icons/icon-logo.tsx
@@ -23,7 +23,7 @@ export default function IconLogo({
     <svg
       width="100%"
       height="100%"
-      viewBox="0 0 48 32"
+      viewBox="0 0 48 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
