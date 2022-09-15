82eb48cb4a59f7f35fe1af52041aac2429ef67d5
Vincent Kraus <mail@vinckr.com>
2022-09-15 15:28:52 +0200

    fix: venue

--- components/venue.tsx
+++ components/venue.tsx
@@ -5,35 +5,65 @@ import styles from './venue.module.css';
 export default function Venue() {
   return (
     <div className={styles.wrapper}>
+      <p>You can attend Ory Summit in person in Munich, Germany this year 🎉</p>
       <p>
-        There are two options to attend the Ory Summit 2022:
+        Due to limited space in the venue, please{' '}
+        <a href="https://forms.gle/mZS9ZxfqcAMvdPkv9">fill out this form</a> if you would like to
+        attend the event in Munich.
+      </p>
+      <p>
+        Ory Summit 2022 takes place in one of Europe's emerging tech hubs - Munich! Enjoy a vibrant
+        city where innovation, technology and rich cultural life come together - right in the heart
+        of Europe. The event is hosted at the Ory Germany Headquarters in the "House of
+        Communication" in the eastern district of Haidhausen.
+      </p>
+      <p>
+        Right next to the Ostbahnhof (Munich East) train station:
         <ul>
           <li>
-            {' '}
-            You can watch the sessions live online at the{' '}
-            <a href="./stage/main">summit.ory.sh Main Stage</a>
+            <a href="https://www.google.com/maps/place/August-Everding-Stra%C3%9Fe+25,+81671+M%C3%BCnchen/">
+              Location on Google Maps
+            </a>
           </li>
-          <li> You attend the event live in the Ory Headquarters in Munich, Germany.</li>
+          <li>5-minute walk from Munich East train station</li>
+          <li>8 minutes by underground tram/S-Bahn train from Munich Central Station </li>
+          <li>approx. 30 minutes by S-Bahn (city commuter train) from Munich Airport </li>
+          <li>2 minutes by car from the Mittlerer Ring ring road </li>
         </ul>
-        Due to limited space in the venue, please send us an{' '}
-        <a href="mailto:office@ory.sh?subject=I want to attend Ory Summit 2022 in-person">email</a>{' '}
-        or <a href="https://forms.gle/mZS9ZxfqcAMvdPkv9">fill out this form</a> if you would like to
-        attend the event live in Munich.
       </p>
-      <p>Ory Summit 2022 takes place in one of Europe's emerging tech hubs - Munich!</p>
-      <p>
-        Enjoy a vibrant city where innovation, technology and rich cultural life come together -
-        right in the heart of Europe.
-      </p>
-      <p>The event is hosted at the Ory Headquarters in the eastern district of Haidhausen. </p>
-
-      <h3 className={styles.info}>Address</h3>
       <div className={styles.info}>
-        <p>August-Everding-Straße 25 / </p>
-        <p>81671 / Munich / Germany</p>
+        <ul>
+          <li>House of Communication</li>
+          <li>August-Everding-Straße 25</li>
+          <li>81671 Munich Germany</li>
+        </ul>
       </div>
+      <div className={styles.imggrid}>
+        <div>
+          <img className={styles.contentimg} src="./summit2.png" alt="Ory Summit 2022" />
+        </div>
+        <div>
+          <img
+            className={styles.contentimg}
+            src="./building.png"
+            alt="House of Communication building"
+          />
+        </div>
+        <div>
+          <img className={styles.contentimg} src="./munich.png" alt="Munich by night" />
+        </div>
+        <div>
+          {' '}
+          <img
+            className={styles.contentimg}
+            src="./plan.png"
+            alt="Map of Munich with an arrow to the House of Communication building"
+          />
+        </div>
+      </div>
+
       <p>
-        For all general inquiries, please contact: <a href="mailto:office@ory.sh">office@ory.sh</a>
+        For all inquiries, please contact: <a href="mailto:office@ory.sh">office@ory.sh</a>
       </p>
     </div>
   );
