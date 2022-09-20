ffe47f2f36d47a7eaab175543318925eb7fec2ff
Vincent Kraus <mail@vinckr.com>
2022-09-20 18:03:05 +0200

    fix: consent text

--- components/consent.tsx
+++ components/consent.tsx
@@ -4,7 +4,8 @@ export default function Consent() {
   return (
     <div className={styles.wrapper}>
       <p className={styles.infotiny}>
-        *by signing up you agree to the <a href="https://www.ory.sh/tos/">Ory Terms of Service</a>.
+        *by signing up you agree to the{' '}
+        <a href="https://www.ory.sh/tos/">Ory Corp Terms of Service</a>.
       </p>
     </div>
   );
