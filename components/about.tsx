606a604fb46dcf3a3c143ad9f0f4c7c3b2f328f4
Vincent Kraus <mail@vinckr.com>
2022-09-20 10:33:55 +0200

    fix: about text

--- components/about.tsx
+++ components/about.tsx
@@ -6,36 +6,39 @@ export default function About() {
   return (
     <div className={styles.wrapper}>
       <p>
-        Ory Summit is a global, free, and hybrid conference around identity management for the Ory
-        Community - users, developers, contributors, partners, and everyone else who is passionate
-        about the future of cloud networking.
+        Ory Summit is a global, hybrid conference around open source based end-to-end security and
+        zero trust solutions for the Ory Community - customers, developers, maintainers,
+        contributors, and partners.
       </p>
       <p>
-        Topics are focused on but not limited to authentication, authorization, access control, and
-        security.
+        Topics are focused on but not limited to first party data privacy and protection, cloud
+        authentication and authorization, compliance with global regulations such as GDPR,
+        distributed access control, risk management and security best practices.
       </p>
       <h2 className={styles.infosmall}>{DATE}</h2>
       <p className={styles.info}>Starts at {TIME}</p>
       <h2>The themes</h2>
       <ul>
         <li>
-          <strong>Ory implementation -</strong> how to implement Ory into an application/project
+          <strong>Ory Cloud -</strong>How to improve any application with best in class security
+          from Ory
         </li>
         <li>
-          <strong>Ecosystem & Integration -</strong> how to use Ory with third-party solutions
+          <strong>Ecosystem & Integration -</strong> how to use Ory with third-party solutions and
+          migrate IDs
         </li>
         <li>
-          <strong>Community Talks -</strong> learn from Ory Community members
+          <strong>Community Talks -</strong> Learn from the Ory Team and experienced users
         </li>
         <li>
-          <strong>Teams & Workflow -</strong> how Ory is making software teams more effective
+          <strong>Teams & Workflow -</strong> how Ory is making software teams more productive
         </li>
         <li>
           <strong>Concept Talks -</strong> authorization & authentication, open source, cloud
-          architecture & security, and more.
+          architecture, compliance, security, data analytics, and much more.
         </li>
       </ul>
-      <h2>How can i attend</h2>
+      <h2>How can I attend</h2>
       <p>
         There are two options to attend Ory Summit 2022:
         <ul>
@@ -50,11 +53,11 @@ export default function About() {
       </p>
       <h2>Why Ory Summit</h2>
       <ul>
-        <li>Code examples and answers from the experts</li>
-        <li>Expand your network and get in touch with Ory engineers</li>
-        <li>Learn about cloud security best practices</li>
-        <li>Get inspired by tech pioneers and industry leaders</li>
+        <li>Learn about Ory Cloud security best practices</li>
+        <li>Listen to tech pioneers and industry leaders</li>
         <li>Level up your skills by engaging in tech talks delivered by proven experts.</li>
+        <li>Expand your network</li>
+        <li>Learn from code examples and answers from the experts</li>
       </ul>
       <p>
         For all general inquiries, please contact: <a href="mailto:office@ory.sh">office@ory.sh</a>
