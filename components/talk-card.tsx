a97c96df6ce49cee94de1f128e8831a88dbd6f93
Vincent Kraus <mail@vinckr.com>
2022-09-22 11:51:44 +0200

    feat: talks without speakers

--- components/talk-card.tsx
+++ components/talk-card.tsx
@@ -43,7 +43,7 @@ export default function TalkCard({ talk: { title, speaker, start, end }, showTim
     setStartAndEndTime(`${formatDate(start)} – ${formatDate(end)}`);
   }, [end, start]);
 
-  const firstSpeakerLink = `/speakers/${speaker[0].slug}`;
+  const firstSpeakerLink = `/speakers/${speaker[0]?.slug}`;
 
   return (
     <div key={title} className={styles.talk}>
