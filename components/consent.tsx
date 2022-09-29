0c25ec77cf6f7a873133b31e56dc2158763a85f8
jfcurran <john@ory.sh>
2022-09-29 11:37:30 +0200

    update styles of consent

--- components/consent.tsx
+++ components/consent.tsx
@@ -2,9 +2,9 @@ import styles from './consent.module.css';
 
 export default function Consent() {
   return (
-    <div className={styles.wrapper}>
-      <p className={styles.infotiny}>
-        *by signing up you agree to the{' '}
+    <div>
+      <p className={styles.consent}>
+        by signing up you agree to the{' '}
         <a href="https://www.ory.sh/tos/">Ory Corp Terms of Service</a>.
       </p>
     </div>
