8fe81e0e9c051f15cd52f52ea30dd10904a1b1a8
Vincent Kraus <mail@vinckr.com>
2023-04-11 23:14:15 +0200

    try: recovery

--- src/components/TicketCard.tsx
+++ src/components/TicketCard.tsx
@@ -4,6 +4,7 @@ const TicketCard = ({ session = null }) => {
   return (
     <div className="p-4">
       <p>Your ticket ID: {session?.identity.id}</p>
+      <p>Your email: {session?.identity.traits.email}</p>
     </div>
   );
 };
