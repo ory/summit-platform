185b80c54531f19f6ecff916453eb498983004fe
Vincent Kraus <mail@vinckr.com>
2023-04-11 22:15:30 +0200

    try: recovery

--- src/components/EmailSignup.tsx
+++ src/components/EmailSignup.tsx
@@ -1,73 +1,36 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import { useState } from "react";
+import { fetchRecoveryLink } from "../utils";
 
-type FormData = {
-  email: string;
-  agreeMarketing: boolean;
-};
-
-const ORY_SDK_URL = process.env.NEXT_PUBLIC_ORY_SDK_URL;
-const ORY_PAT = process.env.NEXT_PUBLIC_ORY_PAT;
 const EmailSignup = () => {
-  const router = useRouter();
-  const { register, handleSubmit } = useForm<FormData>();
-  const onSubmit = (data: FormData) => {
-    const email = data.email; // assuming the input field name is 'email'
-    fetch(`${ORY_SDK_URL}` + `/admin/identities`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${ORY_PAT}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        schema_id: "preset://email",
-        traits: {
-          email: email,
-        },
-      }),
-    })
-      .then((response) => {
-        console.log("Response:", response);
-        router.push("/please-check-your-email");
-      })
-      .catch((error) => {
-        //If there is a status conflict code 409, the identity already exists and we can just redirect to log in
-        router.push(ORY_SDK_URL + "/ui/login");
-        console.error("Error:", error);
-      });
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const result = await fetchRecoveryLink(email);
+
+    if (result) {
+      // handle success
+      console.log("Recovery link sent successfully");
+    } else {
+      // handle error
+      console.error("Error sending recovery link");
+    }
   };
 
   return (
-    <div className="flex justify-center">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col space-y-4"
-      >
-        <input
-          type="email"
-          placeholder="Enter email to get your ticket"
-          {...register("email", { required: true })}
-          className="p-2 border-2 border-gray-400 focus:border-blue-800 w-64"
-        />
-        <div className="flex items-center">
-          <input
-            type="checkbox"
-            {...register("agreeMarketing")}
-            className="w-4 h-4 text-blue-500"
-          />
-          <label htmlFor="agreeMarketing" className="ml-2">
-            Also sign up to the Ory Newsletter
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="p-2 bg-black text-white hover:bg-blue-800"
-        >
-          Get your free ticket
-        </button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <button type="submit">Send Recovery Link</button>
+    </form>
   );
 };
 
---
+++ src/utils.ts
@@ -0,0 +1,47 @@
+const fetchRecoveryLink = async (email) => {
+  try {
+    const response1 = await fetch(
+      "https://login.vinckr.com/self-service/recovery/browser",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        credentials: "include",
+      }
+    );
+
+    const cookies = response1.headers.get("set-cookie");
+    const data1 = await response1.json();
+    const csrfToken = data1.ui.nodes.find(
+      (node) => node.attributes.name === "csrf_token"
+    ).attributes.value;
+
+    const response2 = await fetch(
+      `https://login.vinckr.com/self-service/recovery?flow=${data1.id}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Cookie: cookies,
+        },
+        body: JSON.stringify({
+          method: "code",
+          email: email,
+          csrf_token: csrfToken,
+        }),
+        credentials: "include",
+      }
+    );
+
+    const data2 = await response2.json();
+
+    return data2;
+  } catch (error) {
+    console.error("Error fetching recovery link:", error);
+  }
+};
+
+export { fetchRecoveryLink };
