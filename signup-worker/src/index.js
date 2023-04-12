d24b365a16ac032c514ef429ba1b6285e351980f
Vincent Kraus <mail@vinckr.com>
2023-04-12 17:49:14 +0200

    fix: CORS headers worker

--- signup-worker/src/index.js
+++ signup-worker/src/index.js
@@ -16,34 +16,29 @@ async function handleRequest(request) {
     });
   } else if (request.method === "POST") {
     const body = await request.json();
-    const email = body.email;
-    //const agreeMarketing = json.get("agreeMarketing");
-
     // Create a new account in Ory Network
-    const createIdentity = async (email) => {
+    const createIdentity = async () => {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_ORY_SDK_URL}/admin/identities`,
+        `${env.NEXT_PUBLIC_ORY_SDK_URL}/admin/identities`,
         {
           method: "POST",
           body: JSON.stringify(body),
           headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_ORY_PAT}`,
+            Authorization: `Bearer ${env.NEXT_PUBLIC_ORY_PAT}`,
             "Content-Type": "application/json",
           },
         }
       );
       return response;
     };
-
-    // Placeholder: Call the CRM service API with the email
-    // const crmServiceResponse = await fetch("https://crm-service-api.com", {
-    //   method: "POST",
-    //   body: JSON.stringify({ email, agreeMarketing }),
-    //   headers: { "Content-Type": "application/json" },
-    // });
-
-    return new Response(createIdentity);
-  } else {
-    return new Response("Method not allowed", { status: 405 });
+    const identityResponse = await createIdentity(); // await the createIdentity function
+    return new Response(identityResponse.body, {
+      // Return the response from the createIdentity function
+      status: identityResponse.status,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+        "Content-Type": "application/json",
+      },
+    });
   }
 }
