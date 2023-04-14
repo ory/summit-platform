01a8c1fc2427d7d0623c5f85bb8e1675da75222f
Vincent Kraus <mail@vinckr.com>
2023-04-14 17:33:13 +0200

    fix: about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,10 +16,19 @@ const About: NextPage = () => {
         <p className="text-xl ">Save the date: November 9, 2023</p>
         <br />
         <p className="mb-4">
+          Ory Summit is a global, hybrid conference around open source and zero
+          trust based end-to-end identity management and API security solutions.
+        </p>
+        <p>
+          Its theme: &quot;Securing the digital world&quot;. Topics are focused
+          on but not limited to cloud authentication and authorization,
+          distributed access control, risk management and security best
+          practices.
+        </p>
+        <p>
           Building on the growing popularity Ory Summit is going to take place
           for the third time at the House of Communication in Munich&apos;s
-          trendy Werksviertel and online on 9 November 2023. Its theme:
-          &quot;Securing the digital world&quot;.
+          trendy Werksviertel and online on 9 November 2023.
         </p>
         <br />
         <p>
