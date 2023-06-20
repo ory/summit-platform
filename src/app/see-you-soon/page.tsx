2f6d1beee0c5336ad95ddb6ab8fbe41316f731c1
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-06-20 16:54:26 +0200

    feat: improve post-submission site

--- src/app/see-you-soon/page.tsx
+++ src/app/see-you-soon/page.tsx
@@ -1,7 +1,12 @@
 import classNames from "classnames"
+import Link from "next/link"
+import { Button } from "../components/Button"
 import { Container } from "../components/Container"
+import { Content } from "../components/Content"
+import Countdown from "../components/Countdown"
 import { dividerStyles } from "../components/DividerStyles"
 import { Wrapper } from "../components/Wrapper"
+import { summitStartingDate } from "../startingDate"
 
 export default function SeeYouSoon() {
   return (
@@ -12,7 +17,23 @@ export default function SeeYouSoon() {
       )}
     >
       <Container className="w-full max-w-[1344px]">
-        <Wrapper>See you soon!</Wrapper>
+        <Wrapper>
+          <Content className="col-span-full max-w-[420px]">
+            <div className="flex flex-col gap-4 md:gap-6">
+              <h1 className="text-4xl font-medium uppercase leading-tight">
+                See you soon!
+              </h1>
+              <p>
+                We{"'"}ll be in touch shortly with more information. Keep an eye
+                on your emails!
+              </p>
+            </div>
+            <Button className="self-start" as={Link} href="/">
+              Back to overview
+            </Button>
+            <Countdown targetDate={summitStartingDate} />
+          </Content>
+        </Wrapper>
       </Container>
     </main>
   )
