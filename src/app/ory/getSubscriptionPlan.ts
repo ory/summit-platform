fbd7c8aa3564691ac8bf57bb29ef1d95b1b105b9
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-07-19 17:48:06 +0200

    feat: include project-specific user menu items

--- src/app/ory/getSubscriptionPlan.ts
+++ src/app/ory/getSubscriptionPlan.ts
@@ -1,32 +1,12 @@
 import { Subscription } from "@ory/client"
-import { headers } from "next/headers"
-import "server-only"
-import { getPlanFromSubscription, Plan } from "./SubscriptionPlan"
-import { getNormalizedProject } from "./getNormalizedProject"
 
-const getSubscription = async (): Promise<Subscription | undefined> => {
-  const project = await getNormalizedProject()
-  const subscriptionId = project?.subscription_id
-  if (!subscriptionId) {
-    return undefined
-  }
-
+export const getSubscription = async (subscriptionId: string): Promise<Subscription | undefined> => {
   try {
-    const Cookie = headers().get("Cookie")
-    const requestHeaders = Cookie ? { Cookie } : undefined
     const res = await fetch(
-      process.env.ORY_API_URL + "/subscription/" + subscriptionId,
-      {
-        headers: requestHeaders,
-      },
+      process.env.NEXT_PUBLIC_ORY_API_URL + "/subscription/" + subscriptionId,
     )
     return await res.json()
   } catch (e) {
     return undefined
   }
-}
-
-export const getSubscriptionPlan = async (): Promise<Plan> => {
-  const subscription = await getSubscription()
-  return getPlanFromSubscription(subscription).plan
 }
