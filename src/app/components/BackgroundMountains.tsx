4678a995b691b5a9b131725dac8e0bf31e8300ce
Miłosz <12242002+mszekiel@users.noreply.github.com>
2023-07-20 17:59:04 +0200

    chore: video and cleanup (#50)

--- src/app/components/BackgroundMountains.tsx
+++ src/app/components/BackgroundMountains.tsx
@@ -1,10 +1,12 @@
 "use client"
 
+import bgDarkStill from "@/assets/background-dark-still.webp"
+import bgLightStill from "@/assets/background-light-still.webp"
 import {
   usePreferredColorScheme,
   usePrefersReducedMotion,
 } from "@/hooks/useMatchesMediaQuery"
-import Image from "next/image"
+import dynamic from "next/dynamic"
 import { PropsWithChildren, useEffect, useState } from "react"
 import { P, match } from "ts-pattern"
 
@@ -22,24 +24,35 @@ export const BackgroundMountains = () => {
   const theme = usePreferredColorScheme()
   const prefersReducedMotion = usePrefersReducedMotion()
 
+  const placeholder = match([theme, prefersReducedMotion])
+    .with(["light", P._], () => bgLightStill)
+    .with(["dark", P._], () => bgDarkStill)
+    .exhaustive()
+
   const src = match([theme, prefersReducedMotion])
-    .with(["light", false], () => "/background-light-animated.webp")
-    .with(["dark", false], () => "/background-dark-animated.webp")
-    .with(["light", P._], () => "/background-light-still.webp")
-    .with(["dark", P._], () => "/background-dark-still.webp")
+    .with(["light", P._], () => "/background-light-animated.mp4")
+    .with(["dark", P._], () => "/background-dark-animated.mp4")
     .exhaustive()
 
   return (
     <ClientSideOnly>
-      <Image
-        width={1920}
-        height={1080}
-        src={src}
-        alt=""
-        loading="eager"
-        quality={80}
-        className="absolute inset-0 -z-10 h-full w-full select-none object-cover"
-      />
+      <div className="absolute -inset-y-[1px] inset-x-0 h-full w-full overflow-hidden object-fill">
+        <video
+          autoPlay
+          playsInline
+          muted
+          loop
+          preload="none"
+          className="h-full w-full select-none object-cover"
+          poster={placeholder.src}
+        >
+          <source src={src} type="video/mp4" />
+        </video>
+      </div>
     </ClientSideOnly>
   )
 }
+
+export default dynamic(() => Promise.resolve(BackgroundMountains), {
+  ssr: false,
+})
