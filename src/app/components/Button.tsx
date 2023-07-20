33852224607b5c7f96e85b322b11ff1e6c8d6611
John <johnfitzgeraldcurran@gmail.com>
2023-07-20 14:05:57 +0200

    feat: update button hover style (#48)

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -17,7 +17,7 @@ export const Button = <C extends "a" | "button" | typeof Link = "button">({
     <Component
       className={cn(
         "flex min-w-min gap-2 border border-blue-500 bg-blue-500 px-5 py-[10px] font-normal text-white dark:border-rose-500 dark:bg-rose-500",
-        "hover:border-blue-500 hover:bg-rose-100 hover:text-blue-500 dark:hover:border-rose-500 dark:hover:bg-gray-900 dark:hover:text-rose-500",
+        "hover:border-blue-500 hover:bg-gray-100 hover:text-blue-500 dark:hover:border-rose-500 dark:hover:bg-gray-900 dark:hover:text-rose-500",
         className,
       )}
       {...rest}
