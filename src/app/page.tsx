690259a17573302c4ac8e4ecea1b5eb6dbcc625d
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-08-16 11:10:22 +0200

    fix: preload fallback background images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import bgDarkStill from "@/assets/background-dark-still.webp"
+import bgLightStill from "@/assets/background-light-still.webp"
 import { cn } from "@/utils/cn"
+import { IconDescriptor } from "next/dist/lib/metadata/types/metadata-types"
 import { About } from "./blocks/about"
 import { Hero } from "./blocks/hero"
 import { dividerStyles } from "./components/dividerStyles"
+
+// Instruct browser to preload both background images to make theme switches smoother
+export const metadata = {
+  icons: {
+    other: [
+      {
+        rel: "preload",
+        url: bgLightStill.src,
+        as: "image",
+      } as IconDescriptor,
+      {
+        rel: "preload",
+        url: bgDarkStill.src,
+        as: "image",
+      } as IconDescriptor,
+    ],
+  },
+}
 
 export default function Page() {
   const paddingClassnames = "p-6 sm:p-12 md:py-24 lg:p-24"
