be18f103a80b764c96faa8ee5ae91817e299c52a
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-08-17 14:32:12 +0200

    feat: allow speakers to overflow container

--- src/app/blocks/speakers.tsx
+++ src/app/blocks/speakers.tsx
@@ -2,6 +2,7 @@ import { Container } from "@/app/components/container"
 import { Content } from "@/app/components/content"
 import { Overline } from "@/app/components/overline"
 import { Wrapper } from "@/app/components/wrapper"
+import { cn } from "@/utils/cn"
 
 const speakers = [
   {
@@ -23,11 +24,11 @@ const speakers = [
 ]
 
 export const Speakers = () => (
-  <Container>
+  <Container className="max-w-[--ory-max-content-width]">
     <Wrapper>
       <Content className="col-span-full">
         <div className="flex flex-col gap-2">
-          <Overline>speakers</Overline>
+          <Overline href="#speakers">speakers</Overline>
           <h2 className="text-3xl font-medium uppercase leading-tight">
             The lineup 2023
           </h2>
@@ -38,16 +39,23 @@ export const Speakers = () => (
         </div>
       </Content>
     </Wrapper>
-    <div className="col-span-full flex gap-4 overflow-auto">
+    <div
+      className={cn(
+        // width should overrule right padding -> subtract it manually from screen width
+        "[--total-padding:calc(var(--ory-container-padding)+var(--ory-global-padding))]",
+        "w-[calc(100vw-var(--total-padding))]",
+        "col-span-full flex snap-x snap-mandatory gap-4 overflow-auto",
+      )}
+    >
       {speakers.map((speaker, index) => (
         <div
           key={index}
-          className="flex h-[467px] w-[249px] flex-col justify-between bg-gray-100 p-8"
+          className="flex h-[467px] w-[312px] snap-start flex-col justify-between bg-gray-100 p-8 last:mr-[--total-padding]"
         >
           <img
             src=""
             alt=""
-            className="aspect-square w-[272px] rounded-full border-2 border-gray-900"
+            className="aspect-square w-[272px] max-w-none self-center rounded-full border-2 border-gray-900"
           />
           <div className="h-[114px]">
             <p className="flex flex-col gap-2 leading-normal">
