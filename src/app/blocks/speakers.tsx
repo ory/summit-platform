219769c8447979a2b5adc7be65033196bcc58321
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-08-17 15:40:59 +0200

    feat: add image & improve scroll buttons

--- src/app/blocks/speakers.tsx
+++ src/app/blocks/speakers.tsx
@@ -5,8 +5,10 @@ import { Content } from "@/app/components/content"
 import { Overline } from "@/app/components/overline"
 import { Wrapper } from "@/app/components/wrapper"
 import { cn } from "@/utils/cn"
-import { useCallback, useRef } from "react"
+import Image from "next/image"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { match } from "ts-pattern"
+import testPortrait from "../../assets/test-portrait.jpg"
 
 const speakers = [
   {
@@ -28,7 +30,7 @@ const speakers = [
 ]
 
 export const Speakers = () => {
-  const speakersContainerRef = useRef<HTMLDivElement>(null)
+  const speakersContainerRef = useRef<HTMLUListElement>(null)
   const useScroller = (direction: "left" | "right") =>
     useCallback(() => {
       const speakersContainer = speakersContainerRef.current
@@ -38,9 +40,7 @@ export const Speakers = () => {
         )
       }
 
-      const children = Array.from(
-        speakersContainer.children,
-      ) as HTMLDivElement[]
+      const children = Array.from(speakersContainer.children) as HTMLElement[]
       const target = match(direction)
         .with("left", () =>
           children
@@ -62,6 +62,28 @@ export const Speakers = () => {
 
   const scrollLeft = useScroller("left")
   const scrollRight = useScroller("right")
+  const [canScrollLeft, setCanScrollLeft] = useState(false)
+  const [canScrollRight, setCanScrollRight] = useState(true)
+
+  useEffect(() => {
+    const speakersContainer = speakersContainerRef.current
+    if (!speakersContainer) {
+      return
+    }
+
+    const rightmostChild = speakersContainer.lastChild as HTMLElement
+    const handler = () => {
+      setCanScrollLeft(speakersContainer.scrollLeft > 0)
+      setCanScrollRight(
+        speakersContainer.scrollLeft + speakersContainer.offsetWidth <
+          rightmostChild.offsetLeft + rightmostChild.offsetWidth,
+      )
+    }
+
+    speakersContainer.addEventListener("scroll", handler)
+    return () => speakersContainer.removeEventListener("scroll", handler)
+    // eslint-disable-next-line
+  }, [speakersContainerRef.current])
 
   return (
     <Container className="max-w-[--ory-max-content-width]">
@@ -73,17 +95,27 @@ export const Speakers = () => {
               The lineup 2023
             </h2>
           </div>
-          <div className="flex gap-4 text-3xl font-medium leading-tight text-blue-500">
-            <button className="disabled:text-blue-300" onClick={scrollLeft}>
-              &lt;-
+          <div className="flex gap-4 text-3xl font-medium leading-tight text-blue-500 dark:text-rose-500">
+            <button
+              className="disabled:text-blue-300 dark:disabled:text-rose-800"
+              onClick={scrollLeft}
+              disabled={!canScrollLeft}
+              aria-label="View previous speaker"
+            >
+              <span aria-hidden>&lt;-</span>
             </button>
-            <button className="disabled:text-blue-300" onClick={scrollRight}>
-              -&gt;
+            <button
+              className="disabled:text-blue-300 dark:disabled:text-rose-800"
+              onClick={scrollRight}
+              disabled={!canScrollRight}
+              aria-label="View next speaker"
+            >
+              <span aria-hidden>-&gt;</span>
             </button>
           </div>
         </Content>
       </Wrapper>
-      <div
+      <ul
         ref={speakersContainerRef}
         className={cn(
           // width should overrule right padding -> subtract it manually from screen width
@@ -93,24 +125,28 @@ export const Speakers = () => {
         )}
       >
         {speakers.map((speaker, index) => (
-          <div
+          <li
             key={index}
-            className="flex h-[467px] w-[312px] snap-start flex-col justify-between bg-gray-100 p-8 last:mr-[--total-padding]"
+            className="flex h-[531px] w-[312px] snap-start flex-col justify-between bg-gray-100 p-8 last:mr-[--total-padding] dark:bg-indigo-900"
           >
-            <img
-              src=""
-              alt=""
-              className="aspect-square w-[272px] max-w-none self-center rounded-full border-2 border-gray-900"
-            />
+            <div className="relative aspect-square w-[272px] self-center overflow-hidden rounded-full border-2 border-gray-900">
+              <div className="absolute inset-0 bg-gray-300" />
+              <Image
+                src={testPortrait}
+                alt={`Profile picture of ${speaker.name}`}
+                className="absolute inset-0 bg-blue-500 object-cover mix-blend-normal"
+              />
+              <div className="absolute inset-0 bg-blue-500 mix-blend-screen dark:bg-rose-500" />
+            </div>
             <div className="h-[114px]">
               <p className="flex flex-col gap-2 leading-normal">
                 <span className="text-2xl font-bold">{speaker.name}</span>
                 <span className="text=xl">{speaker.position}</span>
               </p>
             </div>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </Container>
   )
 }
