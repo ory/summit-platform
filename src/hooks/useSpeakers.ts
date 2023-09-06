59d2bc8624dea8c77bb887052c69722a5650e714
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-09-06 17:23:11 +0200

    chore: update sanity-typed

--- src/hooks/useSpeakers.ts
+++ src/hooks/useSpeakers.ts
@@ -4,16 +4,4 @@ import useSWR from "swr"
 import { client } from "../../sanity/lib/client"
 
 export const useSpeakers = () =>
-  useSWR("speakers", async () =>
-    client.fetch(`*[_type == "speaker"] {
-  _id,
-  name,
-  position,
-  profilePicture {
-    asset->{
-      ...,
-      metadata,
-    }
-  },
-}`),
-  )
+  useSWR("speakers", async () => client.fetch(`*[_type == "speaker"]`))
