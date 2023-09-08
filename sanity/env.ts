7744b5a5c412ed4f2b1115dd3c4818c7d17979aa
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-09-08 12:21:36 +0200

    feat: use sanity CDN in prod

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,7 +11,13 @@ export const projectId = assertValue(
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
 )
 
-export const useCdn = false
+export const useCdn =
+  assertValue(
+    process.env.NEXT_PUBLIC_PRODUCTION,
+    "Missing environment variable: NEXT_PUBLIC_PRODUCTION",
+  ).toLowerCase() !== "false"
+
+console.log({ useCdn })
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
