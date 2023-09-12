37e20fe55f8356de56634bb25b184a16316626a8
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-09-12 13:26:11 +0200

    chore: stop using CDN in prod

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,11 +11,7 @@ export const projectId = assertValue(
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
 )
 
-export const useCdn =
-  assertValue(
-    process.env.NEXT_PUBLIC_PRODUCTION,
-    "Missing environment variable: NEXT_PUBLIC_PRODUCTION",
-  ).toLowerCase() !== "false"
+export const useCdn = false
 
 export const withCredentials =
   assertValue(
