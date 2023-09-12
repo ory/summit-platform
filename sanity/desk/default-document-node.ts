dace3784e2aeeff78eadf7889cb8cf6b4e3dc5b4
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-09-12 16:57:37 +0200

    feat: remove published docs if there are drafts

--- sanity/desk/default-document-node.ts
+++ sanity/desk/default-document-node.ts
@@ -1,23 +1,15 @@
 import { getDeploymentOrigin } from "@/utils/get-deployment-origin"
 import Iframe from "sanity-plugin-iframe-pane"
 import { DefaultDocumentNodeResolver } from "sanity/lib/exports/desk"
-import { match } from "ts-pattern"
 
-export const defaultDocumentNode: DefaultDocumentNodeResolver = (
-  S,
-  { schemaType },
-) => {
+export const defaultDocumentNode: DefaultDocumentNodeResolver = (S) => {
   return S.document().views([
     S.view.form(),
-    ...match(schemaType)
-      .with("speaker", () => [
-        S.view
-          .component(Iframe)
-          .options({
-            url: `${getDeploymentOrigin()}/preview`,
-          })
-          .title("Preview"),
-      ])
-      .otherwise(() => []),
+    S.view
+      .component(Iframe)
+      .options({
+        url: `${getDeploymentOrigin()}/preview`,
+      })
+      .title("Preview"),
   ])
 }
--- src/app/(summit-pages)/preview/PreviewProviders.tsx
+++ src/app/(summit-pages)/preview/PreviewProviders.tsx
@@ -2,12 +2,28 @@
 
 import { Providers } from "@/app/(summit-pages)/providers"
 import { PropsWithChildren } from "react"
+import { collate, SanityDocumentLike } from "sanity"
 import useSWR from "swr"
 import {
   getSpeakers,
   getTalks,
   sanityClient,
 } from "../../../../sanity/lib/sanityClient"
+
+// Copied from
+// https://github.com/sanity-io/sanity/blob/f4b66a8695b30d387a2de97d3737252616854a54/packages/%40sanity/desk-tool/src/panes/documentsListPane/DocumentsListPane.js#L24
+function removePublishedWithDrafts<T extends SanityDocumentLike>(
+  documents: T[],
+): T[] {
+  return collate(documents).map((entry) => {
+    const doc: T = entry.draft || entry.published!
+    return {
+      ...doc,
+      hasPublished: !!entry.published,
+      hasDraft: !!entry.draft,
+    }
+  })
+}
 
 const previewClient = sanityClient.withConfig({
   useCdn: false,
@@ -23,7 +39,10 @@ export const PreviewProviders = ({ children }: PropsWithChildren) => {
   )
 
   return (
-    <Providers speakers={speakers ?? []} talks={talks ?? []}>
+    <Providers
+      speakers={removePublishedWithDrafts(speakers ?? [])}
+      talks={removePublishedWithDrafts(talks ?? [])}
+    >
       {children}
     </Providers>
   )
