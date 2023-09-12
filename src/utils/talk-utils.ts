9bbc1d7ff9c16b1ddf96ba1ee61916b9fd68c386
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-09-12 13:22:01 +0200

    fix: speaker name list for only one speaker

--- src/utils/talk-utils.ts
+++ src/utils/talk-utils.ts
@@ -1,7 +1,10 @@
 import { match } from "ts-pattern"
 import { Talk } from "../../sanity/lib/client"
 
-export const getReadableSpeakerList = (talk: Talk, media?: "twitter") => {
+export const getReadableSpeakerList = (
+  talk: Talk,
+  media?: "twitter",
+): string => {
   const speakerNames = talk.speakers.map(
     (speaker) =>
       match(media)
@@ -10,6 +13,11 @@ export const getReadableSpeakerList = (talk: Talk, media?: "twitter") => {
         )
         .otherwise(() => undefined) ?? speaker.name,
   )
+
+  if (speakerNames.length <= 1) {
+    return speakerNames[0] ?? ""
+  }
+
   const firstSpeakers = speakerNames.slice(0, -1)
   const lastSpeaker = speakerNames.at(-1)
   return [firstSpeakers.join(", "), lastSpeaker].join(" and ")
