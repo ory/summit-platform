071eaabf4eade6adff7deea093664b6de9be9d45
Vincent <vincent@ory.sh>
2023-09-21 13:17:30 +0200

    fix: sort agenda by startTime (#71)

--- src/app/(summit-pages)/blocks/agenda.tsx
+++ src/app/(summit-pages)/blocks/agenda.tsx
@@ -35,56 +35,61 @@ export const Agenda = () => {
         </Content>
       </Wrapper>
       <ul className="col-span-full">
-        {talks?.map((talk) => {
-          const start = new Date(talk.startTime)
-
-          return (
-            <li
-              key={talk._id}
-              className="group cursor-pointer border-b border-solid border-blue-500 px-2 py-8 first:border-t hover:bg-gray-50 dark:border-rose-500 dark:hover:bg-indigo-950"
-            >
-              <Link
-                href={getPermalinkFromTalk(talk)}
-                onClick={getOnClickHandler(talk)}
+        {talks
+          ?.sort((talk1, talk2) => {
+            const startTime1 = new Date(talk1.startTime).getTime()
+            const startTime2 = new Date(talk2.startTime).getTime()
+            return startTime1 - startTime2
+          })
+          .map((talk) => {
+            const start = new Date(talk.startTime)
+            return (
+              <li
+                key={talk._id}
+                className="group cursor-pointer border-b border-solid border-blue-500 px-2 py-8 first:border-t hover:bg-gray-50 dark:border-rose-500 dark:hover:bg-indigo-950"
               >
-                <article className="grid grid-cols-[max-content,1fr] gap-x-8 gap-y-4">
-                  <time
-                    dateTime={start.toString()}
-                    aria-label={start.toString()}
-                    className="self-start border border-blue-500 bg-rose-50 p-1 dark:border-rose-500 dark:bg-transparent"
-                  >
-                    <span aria-hidden>
-                      {start.getHours().toString().padStart(2, "0")}:
-                      {start.getMinutes().toString().padStart(2, "0")}
-                    </span>
-                  </time>
-                  <h3 className="self-center leading-tight">{talk.title}</h3>
-                  <address className="col-start-2">
-                    <ul className="flex flex-col gap-1" aria-label="Speakers">
-                      {talk.speakers.map((speaker) => (
-                        <li
-                          key={speaker._id}
-                          className="flex items-center gap-4"
-                        >
-                          <SanityImage
-                            imageSource={
-                              speaker.profilePicture as SanityImageSource
-                            }
-                            sizes="(min-width: 1px) 24px"
-                            alt=""
-                            aria-hidden
-                            className="aspect-square w-6 rounded-full border border-gray-900 object-cover dark:border-white"
-                          />
-                          {speaker.name}
-                        </li>
-                      ))}
-                    </ul>
-                  </address>
-                </article>
-              </Link>
-            </li>
-          )
-        })}
+                <Link
+                  href={getPermalinkFromTalk(talk)}
+                  onClick={getOnClickHandler(talk)}
+                >
+                  <article className="grid grid-cols-[max-content,1fr] gap-x-8 gap-y-4">
+                    <time
+                      dateTime={start.toString()}
+                      aria-label={start.toString()}
+                      className="self-start border border-blue-500 bg-rose-50 p-1 dark:border-rose-500 dark:bg-transparent"
+                    >
+                      <span aria-hidden>
+                        {start.getHours().toString().padStart(2, "0")}:
+                        {start.getMinutes().toString().padStart(2, "0")}
+                      </span>
+                    </time>
+                    <h3 className="self-center leading-tight">{talk.title}</h3>
+                    <address className="col-start-2">
+                      <ul className="flex flex-col gap-1" aria-label="Speakers">
+                        {talk.speakers.map((speaker) => (
+                          <li
+                            key={speaker._id}
+                            className="flex items-center gap-4"
+                          >
+                            <SanityImage
+                              imageSource={
+                                speaker.profilePicture as SanityImageSource
+                              }
+                              sizes="(min-width: 1px) 24px"
+                              alt=""
+                              aria-hidden
+                              className="aspect-square w-6 rounded-full border border-gray-900 object-cover dark:border-white"
+                            />
+                            {speaker.name}
+                          </li>
+                        ))}
+                      </ul>
+                    </address>
+                  </article>
+                </Link>
+              </li>
+            )
+          })}
       </ul>
     </Container>
   )
