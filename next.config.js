11234dc252ffcb8bc9d8c82f0d538a7ca148de7f
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-10-02 13:03:30 +0200

    fix: background animation indexing issue

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async headers() {
+    return [
+      // Prevent Google Search video indexing report for background animations
+      // https://support.google.com/webmasters/answer/9495631#block_page_video
+      {
+        source: "/background-light-animated.webm",
+        headers: [{ key: "X-Robots-Tag", value: "noindex" }],
+      },
+      {
+        source: "/background-dark-animated.webm",
+        headers: [{ key: "X-Robots-Tag", value: "noindex" }],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
